ab333e5874be8117d0e30be986c71a44ca90ce07
Gyan Mani Kharel <gyanmani.kharel@alphaventus.com>
2021-07-10 23:13:55 +0545

    Session functionality

---
+++ ProctoringWebApp/Controllers/SessionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProctoringWebApp.Controllers
+{
+    public class SessionController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ ProctoringWebApp/wwwroot/js/session.js
@@ -0,0 +1,61 @@
+﻿
+    // status fields and start button in UI
+    var phraseDiv;
+    var startRecognizeOnceAsyncButton;
+
+    // subscription key and region for speech services.
+    var subscriptionKey, serviceRegion;
+    var SpeechSDK;
+var recognizer;
+var previousAns;
+
+    document.addEventListener("DOMContentLoaded", function () {
+        startRecognizeOnceAsyncButton = document.getElementById("startRecognizeOnceAsyncButton");
+        subscriptionKey = document.getElementById("subscriptionKey");
+        serviceRegion = document.getElementById("serviceRegion");
+        phraseDiv = document.getElementById("phraseDiv");
+        previousAns = phraseDiv.value;
+
+      startRecognizeOnceAsyncButton.addEventListener("click", function () {
+          startRecognizeOnceAsyncButton.disabled = true;
+          phraseDiv.innerHTML = previousAns;
+
+        if (subscriptionKey.value === "" || subscriptionKey.value === "subscription") {
+        alert("Please enter your Microsoft Cognitive Services Speech subscription key!");
+          return;
+        }
+        var speechConfig = SpeechSDK.SpeechConfig.fromSubscription(subscriptionKey.value, serviceRegion.value);
+
+        speechConfig.speechRecognitionLanguage = "en-US";
+        var audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
+        recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+
+        recognizer.recognizeOnceAsync(
+          function (result) {
+                startRecognizeOnceAsyncButton.disabled = false;
+                previousAns = phraseDiv.innerHTML + result.text;
+                phraseDiv.innerHTML = previousAns;
+            window.console.log(result);
+
+            recognizer.close();
+            recognizer = undefined;
+          },
+          function (err) {
+        startRecognizeOnceAsyncButton.disabled = false;
+            phraseDiv.innerHTML += err;
+            window.console.log(err);
+
+            recognizer.close();
+            recognizer = undefined;
+          });
+      });
+
+      if (!!window.SpeechSDK) {
+        SpeechSDK = window.SpeechSDK;
+        startRecognizeOnceAsyncButton.disabled = false;
+
+        document.getElementById('myDIV').style.display = 'block';
+        document.getElementById('warning').style.display = 'none';
+      }
+    });
+ 
