942910a3826c8e34ef191127b14d65149f73e028
Suraj Gahatraj <suraj.gtrj@gmail.com>
2021-07-11 00:21:39 +0545

    question dummy List Added

--- ProctoringWebApp/Controllers/SessionController.cs
+++ ProctoringWebApp/Controllers/SessionController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProctoringWebApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,49 @@ namespace ProctoringWebApp.Controllers
     {
         public IActionResult Index(int id)
         {
-            return View();
+            var questionList = new List<QuestionViewModel>()
+            {
+                new QuestionViewModel()
+                {
+                    Semester=6,
+                    SubjectCode="CSC-204",
+                    QuestionList=new List<QuestionDetails>()
+                    {
+                        new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="What is OOPS?"
+                        },
+                         new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="Explain Polymorphism with suitable example?"
+                        },
+                          new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="What is Encapsulation"
+                        },
+                           new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="How OOPs solve the problem of code duplication?"
+                        },
+                            new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="What do you mean by class and objects?"
+                        },
+                             new QuestionDetails()
+                        {
+                            Id=1,
+                            QuestionDescription="explain about function overloading"
+                        }
+                    }
+                }
+
+            };
+            return View(questionList);
         }
 
         
---
+++ ProctoringWebApp/ViewModels/QuestionViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProctoringWebApp.ViewModels
+{
+    public class QuestionViewModel
+    {
+       
+        public int Semester { get; set; }
+        public string SubjectCode { get; set; }
+
+        public List<QuestionDetails> QuestionList { get; set; }
+    }
+
+
+    public class QuestionDetails
+    {
+        public int Id { get; set; }
+        public string QuestionDescription { get; set; }
+    }
+
+
+
+
+}
